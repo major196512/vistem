bea18ef9e7addc7b7b50b3ba460ade101368aec8
Taeho Choi <major196512@gmail.com>
2020-08-13 18:46:26 +0900

    debug backend(gloo -> nccl)

--- vistem/dist/backend.py
+++ vistem/dist/backend.py
@@ -6,6 +6,8 @@ __all__ = ['get_global_gloo_group']
 @functools.lru_cache()
 def get_global_gloo_group():
     if dist.get_backend() == "nccl":
+        return dist.new_group(backend="nccl")
+    elif dist.get_backend() == "gloo":
         return dist.new_group(backend="gloo")
     else:
         return dist.group.WORLD
