f4466e5d566a4c8395728a47bf35dfc9e97a1e88
Taeho Choi <major196512@gmail.com>
2020-08-13 21:50:51 +0900

    INPUT argument in CFG

--- vistem/config/defaults/__init__.py
+++ vistem/config/defaults/__init__.py
@@ -1,7 +1,7 @@
 from yacs.config import CfgNode as CN
 
 from .loader import _LOADER
-# from .input import _INPUT
+from .input import _INPUT
 from .solver import _SOLVER
 
 _C = CN()
@@ -10,5 +10,5 @@ _C.DEVICE = 'cuda'
 _C.OUTPUT_DIR = './outputs'
 
 _C.LOADER = _LOADER
-# _C.INPUT = _INPUT
+_C.INPUT = _INPUT
 _C.SOLVER = _SOLVER
