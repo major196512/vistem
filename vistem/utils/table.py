2bfa9b8f3eedc9ae1cd41f42bf3decaf1921811d
major196512 <major196512@gmail.com>
2020-08-17 02:46:28 +0900

    Base COCO Evaluator

---
+++ vistem/utils/table.py
@@ -0,0 +1,13 @@
+from tabulate import tabulate
+
+def create_small_table(small_dict):
+    keys, values = tuple(zip(*small_dict.items()))
+    table = tabulate(
+        [values],
+        headers=keys,
+        tablefmt="pipe",
+        floatfmt=".3f",
+        stralign="center",
+        numalign="center",
+    )
+    return table
