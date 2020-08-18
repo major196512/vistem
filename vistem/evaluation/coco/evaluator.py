1454d330a3927f4e8132781412b9e63d58623845
major196512 <major196512@gmail.com>
2020-08-18 18:29:42 +0900

    COCO Instance Evaluator Class

--- vistem/evaluation/coco/evaluator.py
+++ vistem/evaluation/coco/evaluator.py
@@ -19,28 +19,25 @@ from vistem.utils.table import create_small_table
 from vistem.loader import MetadataCatalog
 from vistem.evaluation.default import Evaluator
 
-from .instances import instances_to_coco_json
+from .instances import COCOInstanceEvaluator
 
-class COCOEvaluator(Evaluator):
+class COCOEvaluator(COCOInstanceEvaluator, Evaluator):
     def __init__(self, cfg, distributed=True):
-        self._tasks = self._tasks_from_config(cfg)
         self._distributed = distributed
         self._output_dir = cfg.OUTPUT_DIR
+        if self._output_dir and not os.path.isdir(self._output_dir) : os.makedirs(self._output_dir)
 
         self._cpu_device = torch.device("cpu")
         self._logger = setup_logger(__name__)
 
         dataset_name = cfg.LOADER.TEST_DATASET
-        self._metadata = MetadataCatalog.get(dataset_name)
+        _metadata = MetadataCatalog.get(dataset_name)
 
+        self._category = _metadata.get("category_names")
         with contextlib.redirect_stdout(io.StringIO()):
-            self._coco_api = COCO(self._metadata.json_file)
+            self._coco_api = COCO(_metadata.json_file)
 
-        self._do_evaluation = "annotations" in self._coco_api.dataset
-
-    def _tasks_from_config(self, cfg):
-        tasks = ("bbox",)
-        return tasks
+        super().__init__(cfg)
 
     def reset(self):
         self._predictions = []
@@ -50,8 +47,7 @@ class COCOEvaluator(Evaluator):
             prediction = {"image_id": input["image_id"]}
 
             if "instances" in output:
-                instances = output["instances"].to(self._cpu_device)
-                prediction["instances"] = instances_to_coco_json(instances, input["image_id"])
+                prediction["instances"] = self._instances_to_coco_json(input, output)
             if "proposals" in output:
                 prediction["proposals"] = output["proposals"].to(self._cpu_device)
             self._predictions.append(prediction)
@@ -67,129 +63,16 @@ class COCOEvaluator(Evaluator):
             predictions = self._predictions
 
         if len(predictions) == 0:
-            self._logger.warning("[COCOEvaluator] Did not receive valid predictions.")
+            self._logger.error("[COCOEvaluator] Did not receive valid predictions.")
             return {}
 
         if self._output_dir:
-            if not  os.path.isdir(self._output_dir) : os.makedirs(self._output_dir)
             file_path = os.path.join(self._output_dir, "instances_predictions.pth")
             with open(file_path, "wb") as f:
                 torch.save(predictions, f)
 
         self._results = OrderedDict()
-        self._eval_predictions(predictions)
+        if "instances" in predictions[0] : self._eval_instances(predictions)
 
         # Copy so the caller can do whatever with results
         return copy.deepcopy(self._results)
-
-    def _eval_predictions(self, predictions):
-        self._logger.info("Preparing results for COCO format ...")
-        coco_results = list(itertools.chain(*[x["instances"] for x in predictions]))
-
-        # unmap the category ids for COCO
-        if hasattr(self._metadata, "dataset_id_to_contiguous_id"):
-            reverse_id_mapping = {
-                v: k for k, v in self._metadata.dataset_id_to_contiguous_id.items()
-            }
-            for result in coco_results:
-                category_id = result["category_id"]
-                assert (
-                    category_id in reverse_id_mapping
-                ), "A prediction has category_id={}, which is not available in the dataset.".format(
-                    category_id
-                )
-                result["category_id"] = reverse_id_mapping[category_id]
-
-        if self._output_dir:
-            file_path = os.path.join(self._output_dir, "coco_instances_results.json")
-            self._logger.info("Saving results to {}".format(file_path))
-            with open(file_path, "w") as f:
-                f.write(json.dumps(coco_results))
-                f.flush()
-
-        if not self._do_evaluation:
-            self._logger.info("Annotations are not available for evaluation.")
-            return
-
-        self._logger.info("Evaluating predictions with COCO API...")
-        for task in sorted(self._tasks):
-            coco_eval = (
-                _evaluate_predictions_on_coco(
-                    self._coco_api,
-                    coco_results,
-                    task,
-                )
-                if len(coco_results) > 0
-                else None  # cocoapi does not handle empty results very well
-            )
-
-            res = self._derive_coco_results(
-                coco_eval, task, class_names=self._metadata.get("category_names")
-            )
-            self._results[task] = res
-
-    def _derive_coco_results(self, coco_eval, iou_type, class_names=None):
-        metrics = {
-            "bbox": ["AP", "AP50", "AP75", "APs", "APm", "APl"],
-            "segm": ["AP", "AP50", "AP75", "APs", "APm", "APl"],
-            "keypoints": ["AP", "AP50", "AP75", "APm", "APl"],
-        }[iou_type]
-
-        if coco_eval is None:
-            self._logger.warn("No predictions from the model!")
-            return {metric: float("nan") for metric in metrics}
-
-        # the standard metrics
-        results = {
-            metric: float(coco_eval.stats[idx] * 100 if coco_eval.stats[idx] >= 0 else "nan")
-            for idx, metric in enumerate(metrics)
-        }
-        self._logger.info(
-            "Evaluation results for {}: \n".format(iou_type) + create_small_table(results)
-        )
-        if not np.isfinite(sum(results.values())):
-            self._logger.info("Some metrics cannot be computed and is shown as NaN.")
-
-        if class_names is None or len(class_names) <= 1:
-            return results
-            
-        precisions = coco_eval.eval["precision"]
-        # precision has dims (iou, recall, cls, area range, max dets)
-        assert len(class_names) == precisions.shape[2]
-
-        results_per_category = []
-        for idx, name in enumerate(class_names):
-            # area range index 0: all area ranges
-            # max dets index -1: typically 100 per image
-            precision = precisions[:, :, idx, 0, -1]
-            precision = precision[precision > -1]
-            ap = np.mean(precision) if precision.size else float("nan")
-            results_per_category.append(("{}".format(name), float(ap * 100)))
-
-        # tabulate it
-        N_COLS = min(6, len(results_per_category) * 2)
-        results_flatten = list(itertools.chain(*results_per_category))
-        results_2d = itertools.zip_longest(*[results_flatten[i::N_COLS] for i in range(N_COLS)])
-        table = tabulate(
-            results_2d,
-            tablefmt="pipe",
-            floatfmt=".3f",
-            headers=["category", "AP"] * (N_COLS // 2),
-            numalign="left",
-        )
-        self._logger.info("Per-category {} AP: \n".format(iou_type) + table)
-
-        results.update({"AP-" + name: ap for name, ap in results_per_category})
-        return results
-
-def _evaluate_predictions_on_coco(coco_gt, coco_results, iou_type):
-    assert len(coco_results) > 0
-
-    coco_dt = coco_gt.loadRes(coco_results)
-    coco_eval = COCOeval(coco_gt, coco_dt, iou_type)
-
-    coco_eval.evaluate()
-    coco_eval.accumulate()
-    coco_eval.summarize()
-
-    return coco_eval
--- vistem/evaluation/coco/instances.py
+++ vistem/evaluation/coco/instances.py
@@ -1,42 +1,151 @@
+import numpy as np
+import pycocotools.mask as mask_util
+
 from vistem.structures import BoxMode
 
-def instances_to_coco_json(instances, img_id):
-    num_instance = len(instances)
-    if num_instance == 0:
-        return []
+from vistem.evaluation.default import Evaluator
 
-    boxes = instances.pred_boxes.tensor.numpy()
-    boxes = BoxMode.convert(boxes, BoxMode.XYXY_ABS, BoxMode.XYWH_ABS)
-    boxes = boxes.tolist()
-    scores = instances.scores.tolist()
-    classes = instances.pred_classes.tolist()
 
-    has_mask = instances.has("pred_masks")
-    if has_mask:
-        # use RLE to encode the masks, because they are too large and takes memory
-        # since this evaluator stores outputs of the entire dataset
-        rles = [
-            mask_util.encode(np.array(mask[:, :, None], order="F", dtype="uint8"))[0]
-            for mask in instances.pred_masks
-        ]
-        for rle in rles:
-            # "counts" is an array encoded by mask_util as a byte-stream. Python3's
-            # json writer which always produces strings cannot serialize a bytestream
-            # unless you decode it. Thankfully, utf-8 works out (which is also what
-            # the pycocotools/_mask.pyx does).
-            rle["counts"] = rle["counts"].decode("utf-8")
 
-    has_keypoints = instances.has("pred_keypoints")
-    if has_keypoints:
-        keypoints = instances.pred_keypoints
+class COCOInstanceEvaluator(Evaluator):
+    def __init__(self, cfg):
+        self._tasks = self._tasks_from_config(cfg)
 
-    results = []
-    for k in range(num_instance):
-        result = {
-            "image_id": img_id,
-            "category_id": classes[k],
-            "bbox": boxes[k],
-            "score": scores[k],
+        self.VISTEM_BOX_MODE = BoxMode.XYXY_ABS
+        self.COCO_BOX_MODE = BoxMode.XYWH_ABS
+
+    def _tasks_from_config(self, cfg):
+        tasks = ("bbox",)
+        return tasks
+
+    def _instances_to_coco_json(self, input, output):
+        img_id = input["image_id"]
+        instances = output["instances"].to(self._cpu_device)
+
+        num_instance = len(instances)
+        if num_instance == 0 : return []
+
+        results = []
+        scores = instances.scores.tolist()
+        classes = instances.pred_classes.tolist()
+        for k in range(num_instance):
+            result = {
+                "image_id": img_id,
+                "category_id": classes[k],
+                "score": scores[k],
+            }
+            results.append(result)
+
+        has_box = instances.has('pred_boxes')
+        has_mask = instances.has("pred_masks")
+        has_keypoint = instances.has("pred_keypoints")
+
+        # BBox Prediction
+        if has_box:
+            boxes = instances.pred_boxes.tensor.numpy()
+            boxes = BoxMode.convert(boxes, self.VISTEM_BOX_MODE, self.COCO_BOX_MODE)
+            boxes = boxes.tolist()
+            for k in range(num_instance):
+                results[k]['bbox'] = boxes[k]
+
+        # Segmentation Prediction
+        if has_mask:
+            rles = [
+                mask_util.encode(np.array(mask[:, :, None], order="F", dtype="uint8"))[0]
+                for mask in instances.pred_masks
+            ]
+            for rle in rles:
+                rle["counts"] = rle["counts"].decode("utf-8")
+            for k in range(num_instance):
+                results[k]["segmentation"] = rles[k]
+
+        # Keypoint Prediction
+        if has_keypoint:
+            keypoints = instances.pred_keypoints
+            keypoints[:][:, :2] -= 0.5
+            for k in range(num_instance):
+                results[k]["keypoints"] = keypoints[k].flatten().tolist()
+
+        return results
+
+    def _eval_instances(self, predictions):
+        self._logger.info("Preparing results for COCO format ...")
+        coco_results = list(itertools.chain(*[x["instances"] for x in predictions]))
+        if len(coco_results) == 0:
+            self._logger.warn("No predictions from the model!")
+            return
+
+        if hasattr(self._metadata, "dataset_id_to_contiguous_id"):
+            reverse_id_mapping = {
+                v: k for k, v in self._metadata.dataset_id_to_contiguous_id.items()
+            }
+            for result in coco_results:
+                category_id = result["category_id"]
+                assert category_id in reverse_id_mapping, f"A prediction has category_id={category_id}, which is not available in the dataset."
+                result["category_id"] = reverse_id_mapping[category_id]
+
+        if self._output_dir:
+            file_path = os.path.join(self._output_dir, "coco_instances_results.json")
+            self._logger.info("Saving results to {}".format(file_path))
+            with open(file_path, "w") as f:
+                f.write(json.dumps(coco_results))
+                f.flush()
+
+        if "annotations" not in self._coco_api.dataset:
+            self._logger.error("Annotations are not available for evaluation.")
+            return
+
+        self._logger.info("Evaluating predictions with COCO API...")
+        for task in sorted(self._tasks):
+            coco_gt = self._coco_api
+            coco_dt = coco_gt.loadRes(coco_results)
+            coco_eval = COCOeval(coco_gt, coco_dt, task)
+
+            coco_eval.evaluate()
+            coco_eval.accumulate()
+            coco_eval.summarize()
+
+            res = self._print_coco_results(coco_eval, task, class_names=self._category)
+            self._results[task] = res
+
+    def _print_coco_results(self, coco_eval, iou_type, class_names=None):
+        metrics = {
+            "bbox": ["AP", "AP50", "AP75", "APs", "APm", "APl"],
+            "segm": ["AP", "AP50", "AP75", "APs", "APm", "APl"],
+            "keypoints": ["AP", "AP50", "AP75", "APm", "APl"],
+        }[iou_type]
+
+        # precision has dims (iou, recall, cls, area range, max dets)    
+        precisions = coco_eval.eval["precision"]
+        assert len(class_names) == precisions.shape[2]
+
+        # the standard metrics
+        results = {
+            metric: float(coco_eval.stats[idx] * 100 if coco_eval.stats[idx] >= 0 else "nan")
+            for idx, metric in enumerate(metrics)
         }
-        results.append(result)
-    return results
+        if not np.isfinite(sum(results.values())):
+            self._logger.info("Some metrics cannot be computed and is shown as NaN.")
+        table = create_small_table(results)
+        self._logger.info(f"Evaluation results for {iou_type}: \n{table}")
+
+        if class_names is None or len(class_names) <= 1:
+            return results
+
+        # metrics per category
+        category_results = []
+        for idx, name in enumerate(class_names):
+            precision = precisions[:, :, idx, 0, -1]
+            precision = precision[precision > -1]
+            ap = np.mean(precision) if precision.size else float("nan")
+            category_results.append((name, float(ap * 100)))
+
+        N_COLS = min(3, len(class_names))
+        results_flatten = list(itertools.chain(*category_results))
+        results_flatten = itertools.zip_longest(*[results_flatten[i::N_COLS] for i in range(N_COLS)])
+
+        table = create_multi_column_table(results_flatten, 3, headers=["category", "AP"], align='left')
+        self._logger.info(f"Per-category {iou_type} AP: \ntable")
+        
+        results.update({f"AP-{name}": ap for name, ap in cagegory_results})
+        return results
