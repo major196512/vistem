c31f0a0374d089334bcfb20345fdfda6ef745665
Taeho Choi <major196512@gmail.com>
2020-08-21 04:11:05 +0900

    Dataset Year Information in VOC Metadata

--- vistem/loader/data/pascal_voc/register.py
+++ vistem/loader/data/pascal_voc/register.py
@@ -7,28 +7,29 @@ from .meta import get_pascal_instances_meta
 __all__ = ['register_all_pascal']
 
 _PREDEFINED_SPLITS = {
-    'voc_2007_trainval' : ('VOC2007', 'trainval', True),
-    'voc_2007_train' : ('VOC2007', 'train', True),
-    'voc_2007_val' : ('VOC2007', 'val', False),
-    'voc_2007_test' :  ('VOC2007', 'test', False),
-    'voc_2012_trainval' : ('VOC2012', 'trainval', True),
-    'voc_2012_train' : ('VOC2012', 'train', True),
-    'voc_2012_val' : ('VOC2012', 'val', False),
+    'voc_2007_trainval' : ('VOC2007', 'trainval', 2007, True),
+    'voc_2007_train' : ('VOC2007', 'train', 2007, True),
+    'voc_2007_val' : ('VOC2007', 'val', 2007, False),
+    'voc_2007_test' :  ('VOC2007', 'test', 2007, False),
+    'voc_2012_trainval' : ('VOC2012', 'trainval', 2012, True),
+    'voc_2012_train' : ('VOC2012', 'train', 2012, True),
+    'voc_2012_val' : ('VOC2012', 'val', 2012, False),
 }
 
 
-def register_pascal(name, metadata, data_root, split, filter):
+def register_pascal(name, metadata, data_root, split, year, filter):
     DatasetCatalog.register(name, lambda: load_voc_instances(data_root, split, name, filter))
     MetadataCatalog.get(name).set(
-        data_root=data_root, split=split, evaluator_type="voc", **metadata
+        data_root=data_root, split=split, year=year, evaluator_type="voc", **metadata
     )
 
 def register_all_pascal(root="./data"):
-    for key, (data_root, split, filter) in _PREDEFINED_SPLITS.items():
+    for key, (data_root, split, year, filter) in _PREDEFINED_SPLITS.items():
         register_pascal(
                 key,
                 get_pascal_instances_meta(),
                 os.path.join(root, data_root),
                 split,
+                year,
                 filter=filter
             )
