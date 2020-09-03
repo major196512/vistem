1a53b97aeb4c5d3c97e81f7b631ffe2813381697
Taeho Choi <major196512@gmail.com>
2020-09-03 17:03:26 +0900

    ops in layers(conv, linear)

--- vistem/modeling/layers/__init__.py
+++ vistem/modeling/layers/__init__.py
@@ -1,3 +1,3 @@
-from .conv import Conv2d
+from .ops import Conv2d, Linear
 from .norm import get_norm
 from .nms import batched_nms
---
+++ vistem/modeling/layers/ops.py
@@ -0,0 +1,46 @@
+import torch
+
+class _NewEmptyTensorOp(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, new_shape):
+        ctx.shape = x.shape
+        return x.new_empty(new_shape)
+
+    @staticmethod
+    def backward(ctx, grad):
+        shape = ctx.shape
+        return _NewEmptyTensorOp.apply(grad, shape), None
+        
+class Conv2d(torch.nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        norm = kwargs.pop("norm", None)
+        activation = kwargs.pop("activation", None)
+        super().__init__(*args, **kwargs)
+
+        self.norm = norm
+        self.activation = activation
+
+    def forward(self, x):
+        x = super().forward(x)
+        if self.norm is not None:
+            x = self.norm(x)
+        if self.activation is not None:
+            x = self.activation(x)
+        return x
+
+class Linear(torch.nn.Linear):
+    def forward(self, x):
+        if x.numel() == 0:
+            output_shape = [x.shape[0], self.weight.shape[0]]
+
+            empty = _NewEmptyTensorOp.apply(x, output_shape)
+            if self.training:
+                # This is to make DDP happy.
+                # DDP expects all workers to have gradient w.r.t the same set of parameters.
+                _dummy = sum(x.view(-1)[0] for x in self.parameters()) * 0.0
+                return empty + _dummy
+            else:
+                return empty
+
+        x = super().forward(x)
+        return x
