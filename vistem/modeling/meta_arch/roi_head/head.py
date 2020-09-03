90200596e3294632d563cdc7774988d7e94647ed
Taeho Choi <major196512@gmail.com>
2020-09-03 17:04:31 +0900

    ROI Head with ROI Align

--- vistem/modeling/meta_arch/roi_head/head.py
+++ vistem/modeling/meta_arch/roi_head/head.py
@@ -1,16 +1,22 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 import numpy as np
-from typing import List, Dict, Tuple, Optional
+from typing import List, Dict, Tuple, Union, Optional
 
 from . import ROI_REGISTRY
+from .pooling import ROIPooler
+
 from vistem.modeling import Matcher, subsample_labels
-from vistem.structures import ImageList, Instances, ShapeSpec, Boxes
 from vistem.modeling.model_utils import pairwise_iou
+from vistem.modeling.layers import Conv2d, Linear
+from vistem.modeling.layers.norm import get_norm
 from vistem.modeling.meta_arch.proposal.proposal_utils import add_ground_truth_to_proposals
 
+from vistem.structures import ImageList, Instances, ShapeSpec, Boxes
 from vistem.utils import get_event_storage
+from vistem.utils import weight_init
 
 @ROI_REGISTRY.register()
 class StandardROIHeads(nn.Module):
@@ -18,6 +24,8 @@ class StandardROIHeads(nn.Module):
         super().__init__()
         self.in_features                    = cfg.MODEL.ROI_HEAD.IN_FEATURES
         self.num_classes                    = cfg.MODEL.ROI_HEAD.NUM_CLASSES
+        in_channels                         = [input_shape[f].channels for f in self.in_features]
+        assert len(set(in_channels)) == 1, in_channels
         for feat in self.in_features:
             assert feat in input_shape.keys(), f"'{feat}' is not in backbone({input_shape.keys()})"
 
@@ -25,35 +33,27 @@ class StandardROIHeads(nn.Module):
         self.batch_size_per_image           = cfg.MODEL.ROI_HEAD.BATCH_SIZE_PER_IMAGE
         self.positive_fraction              = cfg.MODEL.ROI_HEAD.POSITIVE_FRACTION
 
+        pooler_resolution = cfg.MODEL.ROI_HEAD.BOX_POOLER_RESOLUTION
+
         self.proposal_matcher = Matcher(
                 cfg.MODEL.ROI_HEAD.IOU_THRESHOLDS,
                 cfg.MODEL.ROI_HEAD.IOU_LABELS,
                 allow_low_quality_matches=False,
             )
 
+        self.box_pooler = ROIPooler(
+            output_size=pooler_resolution,
+            scales=tuple(1.0 / input_shape[k].stride for k in self.in_features),
+            sampling_ratio=cfg.MODEL.ROI_HEAD.BOX_POOLER_SAMPLING_RATIO,
+            pooler_type=cfg.MODEL.ROI_HEAD.BOX_POOLER_TYPE,
+        )
 
-
-
-
-
-        # self.train_on_pred_boxes = cfg.MODEL.ROI_BOX_HEAD.TRAIN_ON_PRED_BOXES
-
-        # self.in_features = self.box_in_features = cfg.MODEL.ROI_HEADS.IN_FEATURES
-        # in_channels = [input_shape[f].channels for f in in_features]
-        # # Check all channel counts are equal
-        # assert len(set(in_channels)) == 1, in_channels
-        # in_channels = in_channels[0]
-
-        # self.box_pooler = ROIPooler(
-        #     output_size=cfg.MODEL.ROI_BOX_HEAD.POOLER_RESOLUTION,
-        #     scales=tuple(1.0 / input_shape[k].stride for k in in_features),
-        #     sampling_ratio=cfg.MODEL.ROI_BOX_HEAD.POOLER_SAMPLING_RATIO,
-        #     pooler_type=cfg.MODEL.ROI_BOX_HEAD.POOLER_TYPE,
-        # )
-        # self.box_head = build_box_head(
-        #     cfg, ShapeSpec(channels=in_channels, height=pooler_resolution, width=pooler_resolution)
-        # )
-        # self.box_predictor = FastRCNNOutputLayers(cfg, box_head.output_shape)
+        self.box_head = BoxHead(
+            cfg, ShapeSpec(channels=in_channels[0], height=pooler_resolution, width=pooler_resolution)
+        )
+        self.box_predictor = FastRCNNOutputLayers(cfg, self.box_head.output_shape)
+        
+        self.train_on_pred_boxes = cfg.MODEL.ROI_HEAD.TRAIN_ON_PRED_BOXES
 
     def forward(
         self,
@@ -69,17 +69,29 @@ class StandardROIHeads(nn.Module):
             proposals = self.get_ground_truth(proposals, targets)
         del targets
 
-        # if self.training:
-        #     losses = self._forward_box(features, proposals)
-        #     return proposals, losses
-        # else:
-        #     pred_instances = self._forward_box(features, proposals)
-        #     pred_instances = self.forward_with_given_boxes(features, pred_instances)
-        #     return pred_instances, {}
+        features = [features[f] for f in self.in_features]
+        box_features = self.box_pooler(features, [x.proposal_boxes for x in proposals])
+        box_features = self.box_head(box_features)
+        predictions = self.box_predictor(box_features)
+        del box_features
 
-        return [], {}
+        if self.training:
+            losses = self.box_predictor.losses(predictions, proposals)
+            # proposals is modified in-place below, so losses must be computed first.
+            if self.train_on_pred_boxes:
+                with torch.no_grad():
+                    pred_boxes = self.box_predictor.predict_boxes_for_gt_classes(
+                        predictions, proposals
+                    )
+                    for proposals_per_image, pred_boxes_per_image in zip(proposals, pred_boxes):
+                        proposals_per_image.proposal_boxes = Boxes(pred_boxes_per_image)
+            return proposals, losses
 
-    
+        else:
+            pred_instances, _ = self.box_predictor.inference(predictions, proposals)
+            pred_instances = self.forward_with_given_boxes(features, pred_instances)
+            return pred_instances, {}
+
     @torch.no_grad()
     def get_ground_truth(
         self, 
@@ -107,43 +119,115 @@ class StandardROIHeads(nn.Module):
         num_bg_samples = []
 
         for proposals_per_image, targets_per_image in zip(proposals, targets):
-            has_gt = len(targets_per_image) > 0
             match_quality_matrix = pairwise_iou(targets_per_image.gt_boxes, proposals_per_image.proposal_boxes) # (#gt, #proposal)
             matched_idxs, matched_labels = self.proposal_matcher(match_quality_matrix)
             del match_quality_matrix
 
-            has_gt = targets_per_image.gt_classes.numel() > 0
+            # ground truth classes
+            has_gt = len(targets_per_image) > 0
             if has_gt:
-                gt_classes = targets_per_image.gt_classes[matched_idxs]
-                gt_classes[matched_labels == 0] = self.num_classes
-                gt_classes[matched_labels == -1] = -1
+                gt_classes_i = targets_per_image.gt_classes[matched_idxs]
+                # Proposals with label 0 are treated as background.
+                gt_classes_i[matched_labels == 0] = self.num_classes
+                # Proposals with label -1 are ignored.
+                gt_classes_i[matched_labels == -1] = -1
             else:
-                gt_classes = torch.zeros_like(matched_idxs) + self.num_classes
+                gt_classes_i = torch.zeros_like(matched_idxs) + self.num_classes
 
             # Sampling
             sampled_fg_idxs, sampled_bg_idxs = subsample_labels(
-                gt_classes, self.batch_size_per_image, self.positive_fraction, self.num_classes
+                gt_classes_i, self.batch_size_per_image, self.positive_fraction, self.num_classes
             )
             sampled_idxs = torch.cat([sampled_fg_idxs, sampled_bg_idxs], dim=0)
-
             proposals_per_image = proposals_per_image[sampled_idxs]
-            proposals_per_image.gt_classes = targets_per_image.gt_classes[sampled_idxs]
 
+            # ground truth classes with sampling
+            proposals_per_image.gt_classes = gt_classes_i[sampled_idxs]
+
+            # ground truth box
             if has_gt:
                 sampled_targets = matched_idxs[sampled_idxs]
                 proposals_per_image.gt_boxes = targets_per_image.gt_boxes[sampled_targets]
-                for (trg_name, trg_value) in targets_per_image.get_fields().items():
-                    if trg_name.startswith("gt_") and not proposals_per_image.has(trg_name):
-                        proposals_per_image.set(trg_name, trg_value[sampled_targets])
             else:
                 proposals_per_image.gt_boxes = Boxes(targets_per_image.gt_boxes.tensor.new_zeros((len(sampled_idxs), 4)))
 
-            num_bg_samples.append((gt_classes == self.num_classes).sum().item())
-            num_fg_samples.append(gt_classes.numel() - num_bg_samples[-1])
             proposals_with_gt.append(proposals_per_image)
+
+            # Storage with foreground and backgrond samples
+            num_bg_samples.append((gt_classes_i == self.num_classes).sum().item())
+            num_fg_samples.append(gt_classes_i.numel() - num_bg_samples[-1])
 
         storage = get_event_storage()
         storage.put_scalar("roi_head/num_fg_samples", np.mean(num_fg_samples))
         storage.put_scalar("roi_head/num_bg_samples", np.mean(num_bg_samples))
 
         return proposals_with_gt
+
+class BoxHead(nn.Module):
+    def __init__(
+        self, cfg,
+        input_shape : ShapeSpec
+    ):
+
+        super().__init__()
+
+        num_conv = cfg.MODEL.ROI_HEAD.BOX_HEAD_NUM_CONV
+        conv_dim = cfg.MODEL.ROI_HEAD.BOX_HEAD_CONV_DIM
+        num_fc = cfg.MODEL.ROI_HEAD.BOX_HEAD_NUM_FC
+        fc_dim = cfg.MODEL.ROI_HEAD.BOX_HEAD_FC_DIM
+
+        conv_dims = [conv_dim] * num_conv
+        fc_dims = [fc_dim] * num_fc
+        assert len(conv_dims) + len(fc_dims) > 0
+
+        conv_norm = cfg.MODEL.ROI_HEAD.BOX_HEAD_NORM
+
+        self._output_size = (input_shape.channels, input_shape.height, input_shape.width)
+
+        # Conv Subnet
+        self.conv_subnet = []
+        for k, conv_dim in enumerate(conv_dims):
+            conv = Conv2d(
+                self._output_size[0],
+                conv_dim,
+                kernel_size=3,
+                padding=1,
+                bias=not conv_norm,
+                norm=get_norm(conv_norm, conv_dim),
+                activation=F.relu,
+            )
+            self.add_module("conv{}".format(k + 1), conv)
+            self.conv_subnet.append(conv)
+            self._output_size = (conv_dim, self._output_size[1], self._output_size[2])
+
+        # FC Subnet
+        self.fc_subnet = []
+        for k, fc_dim in enumerate(fc_dims):
+            fc = Linear(np.prod(self._output_size), fc_dim)
+            self.add_module("fc{}".format(k + 1), fc)
+            self.fc_subnet.append(fc)
+            self._output_size = fc_dim
+
+        # Initialization
+        for layer in self.conv_subnet:
+            weight_init.c2_msra_fill(layer)
+        for layer in self.fc_subnet:
+            weight_init.c2_xavier_fill(layer)
+
+    def forward(self, x):
+        for layer in self.conv_subnet:
+            x = layer(x)
+        if len(self.fc_subnet):
+            if x.dim() > 2:
+                x = torch.flatten(x, start_dim=1)
+            for layer in self.fc_subnet:
+                x = F.relu(layer(x))
+        return x
+
+    @property
+    def output_shape(self) -> ShapeSpec:
+        o = self._output_size
+        if isinstance(o, int):
+            return ShapeSpec(channels=o)
+        else:
+            return ShapeSpec(channels=o[0], height=o[1], width=o[2])
