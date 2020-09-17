1a1d547ee978d5ca1040c2bf294ddaa6f25f3c9f
Taeho Choi <major196512@gmail.com>
2020-09-18 06:48:57 +0900

    Tiny-ImageNet Dataset Loader

--- vistem/loader/data/register_catalog.py
+++ vistem/loader/data/register_catalog.py
@@ -1,6 +1,8 @@
+from .tiny_imagenet import register_all_tiny_imagenet
 from .coco import register_all_coco
 from .pascal_voc import register_all_pascal
 
 root='./data'
+register_all_tiny_imagenet(root)
 register_all_coco(root)
 register_all_pascal(root)
