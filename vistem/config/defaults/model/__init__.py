f249c9150f7d39c8d738c8604c760d2f87000adc
Taeho Choi <major196512@gmail.com>
2020-09-17 14:57:20 +0900

    NAS-FPN Config Files

--- vistem/config/defaults/model/__init__.py
+++ vistem/config/defaults/model/__init__.py
@@ -1,9 +1,13 @@
 from yacs.config import CfgNode as CN
 
 from .anchor import _ANCHOR_GENERATOR
+
 from .fpn import _FPN
+from .nas_fpn import _NAS_FPN
+
 from .resnet import _RESNETS
 from .retinanet import _RETINANET
+
 from .rpn import _RPN
 from .roi import _ROI
 
@@ -17,8 +21,12 @@ _MODEL.PIXEL_MEAN = [103.530, 116.280, 123.675]
 _MODEL.PIXEL_STD = [1.0, 1.0, 1.0]
 
 _MODEL.ANCHOR_GENERATOR = _ANCHOR_GENERATOR
+
 _MODEL.FPN = _FPN
+_MODEL.NAS_FPN = _NAS_FPN
+
 _MODEL.RESNETS = _RESNETS
 _MODEL.RETINANET = _RETINANET
+
 _MODEL.ROI = _ROI
 _MODEL.RPN = _RPN
