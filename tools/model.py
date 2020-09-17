169679ef07e1f66923f0c1d4c880ef07e640a7bb
Taeho Choi <major196512@gmail.com>
2020-09-18 04:52:48 +0900

    The number of Model Parameters and Data Example

--- tools/model.py
+++ tools/model.py
@@ -2,13 +2,18 @@ from vistem import dist
 from vistem.config import get_cfg
 from vistem.utils import setup_logger
 from vistem.engine import launch, default_argument_parser
+
 from vistem.modeling import build_model
+from vistem.loader import build_train_loader, build_test_loader
 
 logger = setup_logger(__name__)
 
 def main(args):
     cfg = get_cfg(args.config_file)
+
     model = build_model(cfg)
+    train_loader = build_train_loader(cfg)
+    test_loader = build_test_loader(cfg)
 
     if dist.is_main_process():
         logger.info(f'Model Structure\n{model}')
@@ -17,6 +22,14 @@ def main(args):
         logger.debug(f'Backbone Output Features : {model.backbone.out_features}')
         logger.debug(f'Backbone Stride : {model.backbone.out_feature_strides}')
         logger.debug(f'Backbone Output Channels : {model.backbone.out_feature_channels}')
+        
+        train_iter = iter(train_loader)
+        input_data = next(train_iter)
+        logger.debug(f'Input Data Structure\n{input_data[0]}')
+
+        total_param = sum(p.numel() for p in model.parameters())
+        logger.debug(f'The Number of Model Parameters : {total_param}')
+
 
 if __name__ == "__main__":
     args = default_argument_parser().parse_args()
