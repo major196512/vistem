c7463280cbcd401291174fd30a61504a20e672dd
Taeho Choi <major196512@gmail.com>
2020-09-19 01:38:06 +0900

    Swish

--- vistem/modeling/layers/__init__.py
+++ vistem/modeling/layers/__init__.py
@@ -1,3 +1,6 @@
 from .ops import Conv2d, Linear
 from .norm import get_norm
 from .nms import batched_nms
+from .drop_connect import drop_connect
+from .swish import MemoryEfficientSwish, Swish
+from .SEblock import SqueezeExcitation2d
---
+++ vistem/modeling/layers/swish.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+__all__ = ['Swish', 'MemoryEfficientSwish']
+
+# An ordinary implementation of Swish function
+class Swish(nn.Module):
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+
+# A memory-efficient implementation of Swish function
+class SwishImplementation(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, i):
+        result = i * torch.sigmoid(i)
+        ctx.save_for_backward(i)
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        i = ctx.saved_tensors[0]
+        sigmoid_i = torch.sigmoid(i)
+        return grad_output * (sigmoid_i * (1 + i * (1 - sigmoid_i)))
+
+class MemoryEfficientSwish(nn.Module):
+    def forward(self, x):
+        return SwishImplementation.apply(x)
