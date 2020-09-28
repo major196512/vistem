87fe7a0a3346ef2435416dc23a73a44298203bdd
Taeho Choi <major196512@gmail.com>
2020-09-28 22:38:20 +0900

    BiFPN Base code

--- vistem/config/defaults/backbone/__init__.py
+++ vistem/config/defaults/backbone/__init__.py
@@ -6,6 +6,7 @@ from .efficientnet import _EFFICIENTNET
 from .fpn import _FPN
 from .nas_fpn import _NAS_FPN
 from .panet import _PANET
+from .bifpn import _BIFPN
 
 _BACKBONE = CN()
 _BACKBONE.NAME = 'RetinaNetFPN'
@@ -16,3 +17,4 @@ _BACKBONE.EFFICIENTNET = _EFFICIENTNET
 _BACKBONE.FPN = _FPN
 _BACKBONE.NAS_FPN = _NAS_FPN
 _BACKBONE.PANET = _PANET
+_BACKBONE.BIFPN = _BIFPN
