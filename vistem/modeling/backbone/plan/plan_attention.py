434aad2e6854c3d70c98aced85b7da5ab0fc0f5c
Taeho Choi <major196512@gmail.com>
2020-10-06 00:50:13 +0900

    PLAN test1

--- vistem/modeling/backbone/plan/plan_attention.py
+++ vistem/modeling/backbone/plan/plan_attention.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 import math
 import re
 
-from vistem.modeling.layers import Conv2d
+from vistem.modeling.layers import Conv2d, unfold_2d, fold_2d
 
 __all__ = ['PLANAttention']
 
@@ -46,43 +46,35 @@ class PLANAttention(nn.Module):
         B, _, H, W = top_feat.shape
         results = []
 
-        top_key         = self.top_key(top_feat).permute(1, 0, 2, 3).split(self.key_dim)
-        top_query       = self.top_query(top_feat).permute(1, 0, 2, 3).split(self.key_dim)
-        top_value       = self.top_value(top_feat).permute(1, 0, 2, 3).split(self.value_dim)
+        top_key         = self.top_key(top_feat).split(self.key_dim, dim=1)
+        top_query       = self.top_query(top_feat).split(self.key_dim, dim=1)
+        top_value       = self.top_value(top_feat).split(self.value_dim, dim=1)
 
-        bot_key         = self.bot_key(bot_feat).permute(1, 0, 2, 3).split(self.key_dim)
-        bot_query       = self.bot_query(bot_feat).permute(1, 0, 2, 3).split(self.key_dim)
-        bot_value       = self.bot_value(bot_feat).permute(1, 0, 2, 3).split(self.value_dim)
+        bot_key         = self.bot_key(bot_feat).split(self.key_dim, dim=1)
+        bot_query       = self.bot_query(bot_feat).split(self.key_dim, dim=1)
+        bot_value       = self.bot_value(bot_feat).split(self.value_dim, dim=1)
 
         top_results = []
         bot_results = []
         for head_idx in range(self.num_heads):
-            top_K = top_key[head_idx].permute(1, 2, 3, 0).reshape(B*H*W, 1, -1) # (B*H*W, 1, 64)
-            top_Q = top_query[head_idx].permute(1, 2, 3, 0).reshape(B*H*W, 1, -1)
-            top_V = top_value[head_idx].permute(1, 2, 3, 0).reshape(B*H*W, 1, -1)
+            top_K = top_key[head_idx].reshape(B, 1, -1, H, W) # (B, 1, C, H, W)
+            top_Q = top_query[head_idx].reshape(B, 1, -1, H, W)
+            top_V = top_value[head_idx].reshape(B, 1, -1, H, W)
 
-            bot_K = bot_key[head_idx].permute(1, 0, 2, 3)
-            bot_K = F.unfold(bot_K, kernel_size=self.erf, padding=(self.erf-1)//2, stride=2)
-            bot_K = bot_K.permute(0, 2, 1).reshape(B*H*W, self.erf*self.erf, -1) # (B*H*W, 9, 64)
+            bot_K = unfold_2d(bot_key[head_idx], kernel_size=self.erf, padding=(self.erf-1)//2, stride=2) # (B, 9, C, H, W)
+            bot_Q = unfold_2d(bot_query[head_idx], kernel_size=self.erf, padding=(self.erf-1)//2, stride=2)
+            bot_V = unfold_2d(bot_value[head_idx], kernel_size=self.erf, padding=(self.erf-1)//2, stride=2)
 
-            bot_Q = bot_query[head_idx].permute(1, 0, 2, 3)
-            bot_Q = F.unfold(bot_Q, kernel_size=self.erf, padding=(self.erf-1)//2, stride=2)
-            bot_Q = bot_Q.permute(0, 2, 1).reshape(B*H*W, self.erf*self.erf, -1) # (B*H*W, 9, 64)
-
-            bot_V = bot_value[head_idx].permute(1, 0, 2, 3)
-            bot_V = F.unfold(bot_V, kernel_size=self.erf, padding=(self.erf-1)//2, stride=2)
-            bot_V = bot_V.permute(0, 2, 1).reshape(B*H*W, self.erf*self.erf, -1) # (B*H*W, 9, 64)
-
-            key = torch.cat([top_K, bot_K], dim=1) # (B*H*W, 10, 64)
+            key = torch.cat([top_K, bot_K], dim=1) # (B, 10, C, H, W)
             query = torch.cat([top_Q, bot_Q], dim=1)
             value = torch.cat([top_V, bot_V], dim=1)
 
-            weight = (key.unsqueeze(2) * query.unsqueeze(1)).sum(dim=-1) / math.sqrt(self.key_dim) # (N, 10, 10)
-            out = (weight.unsqueeze(dim=3) * value.unsqueeze(dim=2)).sum(dim=2) # (N, 10, 64)
+            weight = (key.unsqueeze(2) * query.unsqueeze(1)).sum(dim=3) / math.sqrt(self.key_dim) # (B, 10, 10, H, W)
+            out = (weight.unsqueeze(dim=3) * value.unsqueeze(dim=2)).sum(dim=2) # (B, 10, C, H, W)
 
-            top_ret = out[:, 0, :].view(B, H, W, -1).permute(0, 3, 1, 2) # (B, 64, H, W)
-            bot_ret = out[:, 1:, :].view(B, H*W, -1).permute(0, 2, 1) # (B, 9*64, H*W)
-            bot_ret = F.fold(bot_ret, bot_feat.shape[-2:], kernel_size=self.erf, padding=(self.erf-1)//2, stride=2)
+            top_ret = out[:, 0, :, :, :]
+            bot_ret = out[:, 1:, :, :, :]
+            bot_ret = fold_2d(bot_ret, out_shape=bot_feat.shape[2:], kernel_size=self.erf, padding=(self.erf-1)//2, stride=2)
 
             top_results.append(top_ret)
             bot_results.append(bot_ret)
