0208642dbe708028d7ae812d6f9a9d4353994c98
Taeho Choi <major196512@gmail.com>
2020-10-24 15:04:58 +0900

    mask head temp

--- vistem/modeling/meta_arch/roi_head/head.py
+++ vistem/modeling/meta_arch/roi_head/head.py
@@ -10,7 +10,7 @@ from .pooling import ROIPooler
 
 from vistem.modeling import Box2BoxTransform, Matcher, subsample_labels
 from vistem.modeling.model_utils import pairwise_iou
-from vistem.modeling.layers import Conv2d, Linear, batched_nms
+from vistem.modeling.layers import Conv2d, ConvTranspose2d, Linear, batched_nms
 from vistem.modeling.layers.norm import get_norm
 from vistem.modeling.meta_arch import DefaultMetaArch
 from vistem.modeling.meta_arch.proposal.proposal_utils import add_ground_truth_to_proposals
@@ -24,6 +24,8 @@ from vistem.utils import weight_init
 class StandardROIHeads(DefaultMetaArch):
     def __init__(self, cfg, input_shape):
         super().__init__(cfg)
+
+        self.seg_on                         = cfg.INPUT.SEG_ON
 
         self.in_features                    = cfg.META_ARCH.ROI.IN_FEATURES
         self.num_classes                    = cfg.META_ARCH.NUM_CLASSES
@@ -45,15 +47,32 @@ class StandardROIHeads(DefaultMetaArch):
         self.positive_fraction              = cfg.META_ARCH.ROI.SAMPLING.POSITIVE_FRACTION
 
         # Pooling Parameters and Module
-        box_pooler_type                     = cfg.META_ARCH.ROI.BOX_POOLING.TYPE
-        box_pooler_resolution               = cfg.META_ARCH.ROI.BOX_POOLING.RESOLUTION
-        box_pooler_sampling_ratio           = cfg.META_ARCH.ROI.BOX_POOLING.SAMPLING_RATIO
+        pooler_type                     = cfg.META_ARCH.ROI.POOLING.TYPE
+        pooler_resolution               = cfg.META_ARCH.ROI.POOLING.RESOLUTION
+        pooler_sampling_ratio           = cfg.META_ARCH.ROI.POOLING.SAMPLING_RATIO
+
+        # ROI Box Head
         self.box_pooler = ROIPooler(
-            output_size=box_pooler_resolution,
+            output_size=pooler_resolution,
             scales=tuple(1.0 / input_shape[k].stride for k in self.in_features),
-            sampling_ratio=box_pooler_sampling_ratio,
-            pooler_type=box_pooler_type,
+            sampling_ratio=pooler_sampling_ratio,
+            pooler_type=pooler_type,
         )
+        self.box_head = BoxHead(
+            cfg, ShapeSpec(channels=in_channels[0], height=pooler_resolution, width=pooler_resolution)
+        )
+        # self.train_on_pred_boxes            = cfg.MODEL.ROI_HEAD.TRAIN_ON_PRED_BOXES
+
+        if self.seg_on:
+            self.mask_pooler = ROIPooler(
+                output_size=pooler_resolution,
+                scales=tuple(1.0 / input_shape[k].stride for k in self.in_features),
+                sampling_ratio=pooler_sampling_ratio,
+                pooler_type=pooler_type,
+            )
+            self.mask_head = MaskHead(
+                cfg, ShapeSpec(channels=in_channels[0])
+            )
 
         # Loss parameters
         self.loss_weight                    = cfg.META_ARCH.ROI.BOX_LOSS.LOSS_WEIGHT
@@ -72,12 +91,6 @@ class StandardROIHeads(DefaultMetaArch):
         self.score_threshhold               = cfg.TEST.SCORE_THRESH
         self.max_detections_per_image       = cfg.TEST.DETECTIONS_PER_IMAGE
 
-        # ROI Head
-        self.box_head = BoxHead(
-            cfg, ShapeSpec(channels=in_channels[0], height=box_pooler_resolution, width=box_pooler_resolution)
-        )
-        # self.train_on_pred_boxes            = cfg.MODEL.ROI_HEAD.TRAIN_ON_PRED_BOXES
-    
     def forward(
         self,
         images: ImageList,
@@ -92,12 +105,18 @@ class StandardROIHeads(DefaultMetaArch):
             del targets
         
         features = [features[f] for f in self.in_features]
+
         box_features = self.box_pooler(features, [x.proposal_boxes for x in proposals])
         scores, proposal_deltas = self.box_head(box_features)
         del box_features
 
+        if self.seg_on:
+            mask_features = self.mask_pooler(features, [x.proposal_boxes for x in proposals])
+            mask_features = self.mask_head(mask_features)
+
         if self.training:
-            losses = self.losses(scores, proposal_deltas, proposals)
+            losses = self.losses_box(scores, proposal_deltas, proposals)
+            if self.seg_on : losses.update(self.losses())
 
             # proposals is modified in-place below, so losses must be computed first.
             # if self.train_on_pred_boxes:
@@ -118,7 +137,7 @@ class StandardROIHeads(DefaultMetaArch):
 
             return results, {}
 
-    def losses(
+    def losses_box(
         self, 
         pred_scores : torch.Tensor, 
         pred_deltas : torch.Tensor, 
@@ -170,6 +189,41 @@ class StandardROIHeads(DefaultMetaArch):
         loss_cls *= self.loss_weight.get('loss_cls', 1.0)
         loss_loc *= self.loss_weight.get('loss_loc', 1.0)
         return {"loss_cls": loss_cls, "loss_loc": loss_loc}
+
+    def losses_mask(
+        self, 
+        pred : torch.Tensor,
+        proposals : List[Instances],
+    ):
+        num_masks = pred.size(0)
+        mask_side_len = pred.size(2)
+        assert pred.size(2) == pred.size(3), "Mask prediction must be square!"
+
+        gt_masks = []
+        gt_classes = []
+        for proposal_per_image in proposals:
+            if len(proposal_per_image) == 0 : continue
+            
+            gt_class = proposal_per_image.gt_classes.to(dtype=torch.int64)
+            gt_mask = proposal_per_image.gt_mask.crop_and_resize(
+                proposal_per_image.proposal_boxes.tensor, mask_side_len
+            ).to(device=pred.device)
+
+            gt_classes.append(gt_class)
+            gt_masks.append(gt_mask)
+
+        if len(gt_masks) == 0 : return pred.sum() * 0
+
+        gt_classes = torch.cat(gt_classes, dim=0)
+        gt_masks = torch.cat(gt_masks, dim=0)
+        gt_masks = gt_masks.to(dtype=torch.float32)
+        gt_masks_bool = (gt_masks > 0.5)
+
+        pred = pred[torch.arange(num_masks), gt_classes]
+
+        mask_loss = F.binary_cross_entropy_with_logits(pred, gt_masks, reduction='mean')
+
+        return mask_loss
 
     @torch.no_grad()
     def get_ground_truth(
@@ -227,6 +281,8 @@ class StandardROIHeads(DefaultMetaArch):
             if has_gt:
                 sampled_targets = matched_idxs[sampled_idxs]
                 proposals_per_image.gt_boxes = targets_per_image.gt_boxes[sampled_targets]
+                if hasattr(targets_per_image, 'gt_mask') : 
+                    proposals_per_image.gt_mask = targets_per_image.gt_mask[sampled_targets]
             else:
                 proposals_per_image.gt_boxes = Boxes(targets_per_image.gt_boxes.tensor.new_zeros((len(sampled_idxs), 4)))
 
@@ -385,3 +441,70 @@ class BoxHead(nn.Module):
         proposal_deltas = self.bbox_pred(x)
 
         return scores, proposal_deltas
+
+class MaskHead(nn.Module):
+    def __init__(
+        self, cfg,
+        input_shape : ShapeSpec
+    ):
+
+        super().__init__()
+
+        num_classes     = cfg.META_ARCH.NUM_CLASSES
+
+        num_conv        = cfg.META_ARCH.ROI.MASK_HEAD.NUM_CONV
+        conv_dim        = cfg.META_ARCH.ROI.MASK_HEAD.CONV_DIM
+        conv_norm       = cfg.META_ARCH.ROI.MASK_HEAD.CONV_NORM
+
+        conv_dims = [conv_dim] * num_conv
+
+        output_size = input_shape.channels
+
+        self.conv_subnet = []
+        for k, conv_dim in enumerate(conv_dims):
+            conv = Conv2d(
+                output_size,
+                conv_dim,
+                kernel_size=3,
+                padding=1,
+                stride=1,
+                bias=not conv_norm,
+                norm=get_norm(conv_norm, conv_dim),
+                activation=F.relu,
+            )
+            self.add_module(f'conv{k+1}', conv)
+            self.conv_subnet.append(conv)
+            output_size = conv_dim
+
+        self.deconv = ConvTranspose2d(
+            output_size,
+            conv_dim,
+            kernel_size=2,
+            stride=2,
+            padding=0,
+            activation=F.relu,
+        )
+        output_size = conv_dim
+
+        self.predictor = Conv2d(
+            output_size,
+            num_classes,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+        )
+        # Initialization
+        for layer in self.conv_subnet:
+            weight_init.c2_msra_fill(layer)
+        weight_init.c2_xavier_fill(self.deconv)
+        nn.init.normal_(self.predictor.weight, std=0.001)
+        if self.predictor.bias is not None:
+            nn.init.normal_(self.predictor.bias, 0)
+
+    def forward(self, x):
+        for layer in self.conv_subnet:
+            x = layer(x)
+        x = self.deconv(x)
+        x = self.predictor(x)
+
+        return x
