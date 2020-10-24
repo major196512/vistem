76f77f4685de7499706687d6877efb4985a3c8b6
Taeho Choi <major196512@gmail.com>
2020-10-24 15:04:24 +0900

    pooling in config files

--- vistem/modeling/layers/ops.py
+++ vistem/modeling/layers/ops.py
@@ -28,6 +28,23 @@ class Conv2d(torch.nn.Conv2d):
             x = self.activation(x)
         return x
 
+class ConvTranspose2d(torch.nn.ConvTranspose2d):
+    def __init__(self, *args, **kwargs):
+        norm = kwargs.pop('norm', None)
+        activation = kwargs.pop('activation', None)
+        super().__init__(*args, **kwargs)
+
+        self.norm = norm
+        self.activation = activation
+
+    def forward(self, x):
+        x = super().forward(x)
+        if self.norm is not None:
+            x = self.norm(x)
+        if self.activation is not None:
+            x = self.activation(x)
+
+        return x
 class Linear(torch.nn.Linear):
     def forward(self, x):
         if x.numel() == 0:
